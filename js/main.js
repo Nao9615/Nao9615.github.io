44a75bbb776e093fe59c772a5acbc69623aabc07
Atuatu-Hhakumai <microsoooooft365@gmail.com>
2025-01-19 09:59:34 +0900

    Update

--- js/main.js
+++ js/main.js
@@ -3,11 +3,16 @@ const fullHeight = () => {
   const vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 };
-
 window.addEventListener('resize', fullHeight);
 fullHeight();
 
-// モードに応じてテーマを更新する関数
+
+// カラーテーマ
+
+const themeBtn = document.querySelector("#color-theme-btn");
+const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+// モードに応じてテーマを更新
 function updateTheme(isDarkMode) {
   if (isDarkMode) {
     document.body.classList.remove('light-mode');
@@ -20,36 +25,30 @@ function updateTheme(isDarkMode) {
   }
 }
 
-const themeBtn = document.querySelector("#color-theme-btn");
-const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-
-// 初期設定をチェックして適用
 const savedMode = localStorage.getItem('dark-mode-settings');
 if (savedMode === 'dark' || (savedMode === null && darkModeMediaQuery.matches)) {
   updateTheme(true);
-  themeBtn.checked = true; // チェックボックスを使用している場合、チェックを入れる
+  themeBtn.checked = true;
 } else {
   updateTheme(false);
   themeBtn.checked = false;
 }
 
-// ユーザーの設定変更をリッスン
+// ユーザーの設定変更
 darkModeMediaQuery.addEventListener('change', (e) => {
   updateTheme(e.matches);
 });
 
-// チェックボックスの変更イベント
+// チェックボックス
 themeBtn.addEventListener("change", () => {
   updateTheme(themeBtn.checked);
 });
 
-// 言語を切り替える
+// 言語切り替え
 document.getElementById("Lang-En-Ja").addEventListener("change", function() {
   var en = document.querySelectorAll(".is-en");
   var ja = document.querySelectorAll(".is-ja");
-
   if (this.checked) {
-    // グループ1を非表示にし、グループ2を表示
     en.forEach(function(element) {
         element.classList.add("hidden");
     });
@@ -57,7 +56,6 @@ document.getElementById("Lang-En-Ja").addEventListener("change", function() {
         element.classList.remove("hidden");
     });
   } else {
-    // グループ1を表示し、グループ2を非表示
     en.forEach(function(element) {
         element.classList.remove("hidden");
     });
