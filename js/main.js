5e03bbd0a42b64e3c6f45955378cfb29bebf789e
Atuatu-Hhakumai <microsoooooft365@gmail.com>
2025-07-21 23:53:52 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,31 +1,33 @@
 // 高さをデバイスの画面ピッタリに
 const fullHeight = () => {
   const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
 };
-window.addEventListener('resize', fullHeight);
+window.addEventListener("resize", fullHeight);
 fullHeight();
-
 
 // カラーテーマ
 const themeBtn = document.querySelector("#color-theme-btn");
-const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
 // モードに応じてテーマを更新
 function updateTheme(isDarkMode) {
   if (isDarkMode) {
-    document.body.classList.remove('light-mode');
-    document.body.classList.add('dark-mode');
-    localStorage.setItem('dark-mode-settings', 'dark');
+    document.body.classList.remove("light-mode");
+    document.body.classList.add("dark-mode");
+    localStorage.setItem("dark-mode-settings", "dark");
   } else {
-    document.body.classList.remove('dark-mode');
-    document.body.classList.add('light-mode');
-    localStorage.setItem('dark-mode-settings', 'light');
+    document.body.classList.remove("dark-mode");
+    document.body.classList.add("light-mode");
+    localStorage.setItem("dark-mode-settings", "light");
   }
 }
 
-const savedMode = localStorage.getItem('dark-mode-settings');
-if (savedMode === 'dark' || (savedMode === null && darkModeMediaQuery.matches)) {
+const savedMode = localStorage.getItem("dark-mode-settings");
+if (
+  savedMode === "dark" ||
+  (savedMode === null && darkModeMediaQuery.matches)
+) {
   updateTheme(true);
   themeBtn.checked = true;
 } else {
@@ -34,7 +36,7 @@ if (savedMode === 'dark' || (savedMode === null && darkModeMediaQuery.matches)) 
 }
 
 // ユーザーの設定変更
-darkModeMediaQuery.addEventListener('change', (e) => {
+darkModeMediaQuery.addEventListener("change", (e) => {
   updateTheme(e.matches);
 });
 
@@ -44,22 +46,40 @@ themeBtn.addEventListener("change", () => {
 });
 
 // 言語切り替え
-document.getElementById("Lang-En-Ja").addEventListener("change", function() {
+document.getElementById("Lang-En-Ja").addEventListener("change", function () {
   var en = document.querySelectorAll(".is-en");
   var ja = document.querySelectorAll(".is-ja");
   if (this.checked) {
-    en.forEach(function(element) {
-        element.classList.add("hidden");
+    en.forEach(function (element) {
+      element.classList.add("hidden");
     });
-    ja.forEach(function(element) {
-        element.classList.remove("hidden");
+    ja.forEach(function (element) {
+      element.classList.remove("hidden");
     });
   } else {
-    en.forEach(function(element) {
-        element.classList.remove("hidden");
+    en.forEach(function (element) {
+      element.classList.remove("hidden");
     });
-    ja.forEach(function(element) {
-        element.classList.add("hidden");
+    ja.forEach(function (element) {
+      element.classList.add("hidden");
     });
   }
+});
+
+const btns = document.querySelectorAll(".buttons a");
+
+btns.forEach((btn) => {
+  const btnMask = btn.querySelectorAll(".btn-mask");
+  btn.addEventListener("mousemove", () => {
+    gsap.to(btnMask, {
+      opacity: 1,
+      duration: 0.1,
+    });
+  });
+  btn.addEventListener("mouseleave", () => {
+    gsap.to(btnMask, {
+      opacity: 0,
+      duration: 0.1,
+    });
+  });
 });
